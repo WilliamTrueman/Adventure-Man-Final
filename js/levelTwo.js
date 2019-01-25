94a9098b38327d614e26b5939cbe88d839d32e7d
WilliamTrueman <44092855+WilliamTrueman@users.noreply.github.com>
2019-01-25 12:24:35 +0000

    Update levelTwo.js

--- js/levelTwo.js
+++ js/levelTwo.js
@@ -56,28 +56,28 @@ var levelTwo = {
 
 
 
-        //  We're going to be using physics, so enable the Arcade Physics system
+        
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        //  A simple background for our game
+     
         game.add.sprite(0, 0, 'background');
 
-        //  The platforms group contains the ground and the 2 ledges we can jump on
+        
         platforms = game.add.group();
 
-        //  We will enable physics for any object that is created in this group
+    
         platforms.enableBody = true;
 
-        // Here we create the ground.
+        
         var ground = platforms.create(0, game.world.height - 64, 'ground');
 
-        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+     
         ground.scale.setTo(2, 2);
 
-        //  This stops it from falling away when you jump on it
+     
         ground.body.immovable = true;
 
-        //  Now let's create two ledges
+        
         var ledge = platforms.create(100, 320, 'ground');
         ledge.body.immovable = true;
         ledge.scale.setTo(0.5,1);
@@ -98,15 +98,15 @@ var levelTwo = {
         ledge.scale.setTo(0.5,1);
 
 
-        //  Finally some stars to collect
+        
         stars = game.add.group();
 
-        //  We will enable physics for any star that is created in this group
+      
         stars.enableBody = true;
 
-        //  Here we'll create 12 of them evenly spaced apart
+    
 
-        //  Create a star inside of the 'stars' group
+      
         star = stars.create(140,500,'star');
         star = stars.create(320,500,'star');
         star = stars.create(230,35,'star');
@@ -213,13 +213,13 @@ var levelTwo = {
 
 
 
-        // The player and its settings
+      
         player = game.add.sprite(700, game.world.height -  700, 'dude');
 
-        //  We need to enable physics on the player
+        
         game.physics.arcade.enable(player);
 
-        //  Player physics properties. Give the little guy a slight bounce.
+     .
         player.body.bounce.y = 0.2;
         player.body.gravity.y = 300;
         player.body.collideWorldBounds = true;
