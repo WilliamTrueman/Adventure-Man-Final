34bc46cf4e86291bf87f02ea103563cfc9eebdcf
WilliamTrueman <44092855+WilliamTrueman@users.noreply.github.com>
2019-01-25 12:26:25 +0000

    Update levelThree.js

--- js/levelThree.js
+++ js/levelThree.js
@@ -66,25 +66,25 @@ var levelThree = {
         audio.play();
 
 
-        //  We're going to be using physics, so enable the Arcade Physics system
+        
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        //  A simple background for our game
+   
         game.add.sprite(0, 0, 'background');
 
-        //  The platforms group contains the ground and the 2 ledges we can jump on
+        
         platforms = game.add.group();
 
-        //  We will enable physics for any object that is created in this group
+       
         platforms.enableBody = true;
 
-        // Here we create the ground.
+     
         var ground = platforms.create(717, game.world.height - 64, 'ground');
 
-        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+   
         ground.scale.setTo(0.5, 2);
 
-        //  This stops it from falling away when you jump on it
+       
         ground.body.immovable = true;
 
 
@@ -105,7 +105,7 @@ var levelThree = {
         water.body.immovable = true;
 
 
-        //  Now let's create two ledges
+      
         var ledge = platforms.create(100, 320, 'ground');
         ledge.body.immovable = true;
         ledge.scale.setTo(0.5,1);
@@ -140,15 +140,13 @@ var levelThree = {
         ledge.body.immovable = true;
         ledge.scale.setTo(0.5,1);
 
-        //  Finally some stars to collect
+      
         stars = game.add.group();
 
-        //  We will enable physics for any star that is created in this group
+       
         stars.enableBody = true;
 
-        //  Here we'll create 12 of them evenly spaced apart
-
-        //  Create a star inside of the 'stars' group
+        
 
         star = stars.create(320,55,'star');
         star.body.gravity.y = 300;
@@ -244,18 +242,18 @@ var levelThree = {
 
 
 
-        // The player and its settings
+        
         player = game.add.sprite(730, game.world.height -  150, 'dude');
 
-        //  We need to enable physics on the player
+    
         game.physics.arcade.enable(player);
 
-        //  Player physics properties. Give the little guy a slight bounce.
+     
         player.body.bounce.y = 0.2;
         player.body.gravity.y = 300;
         player.body.collideWorldBounds = true;
 
-        //  Our two animations, walking left and right.
+        
         player.animations.add('left', [0, 1, 2, 3], 10, true);
         player.animations.add('right', [5, 6, 7, 8], 10, true);
 
